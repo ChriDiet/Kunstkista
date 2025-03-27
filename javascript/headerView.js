a44a2b78ba7920213b813f56e08c3733c9f79375
Christina <ch.dietrichs@yahoo.no>
2025-03-27 14:13:51 +0100

    New pages, minor changes to index

---
+++ javascript/headerView.js
@@ -0,0 +1,19 @@
+function createHeader() {
+   return /*HTML*/ `
+   <div class="header">
+   <div class="logo">Kunstkista</div>
+   <div class="menu">
+      <p>Hjem</p>
+      <p>Auksjon</p>
+      <p>Kategorier</p>
+   </div>
+   <div class="searchbar">
+      <input type="search" id="searchbar"/>
+   </div>
+   <div class="userprofile">
+      <img src="placeholder.png"/>
+      Logg inn
+   </div>
+</div>
+   `;
+}
