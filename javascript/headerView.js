46f3d15a3518fc40e7e43e208b10ec9ae90221ee
Christina <ch.dietrichs@yahoo.no>
2025-03-28 13:09:11 +0100

    Create headerView

--- javascript/headerView.js
+++ javascript/headerView.js
@@ -1,17 +1,19 @@
 function createHeader() {
    return /*HTML*/ `
    <div class="header">
-   <div class="logo">Kunstkista</div>
-   <div class="menu">
-      <p>Hjem</p>
-      <p>Auksjon</p>
-      <p>Kategorier</p>
+      <div class="logo">Kunstkista</div>
+   <div class="menuButtons">
+      <button onclick="showHomeView()">
+         <img class="homeButton" src="./images/home.png"/>
+      </button>
+      <button>Auksjon</button>
+      <button>Kategorier</button>
    </div>
    <div class="searchbar">
-      <input type="search" id="searchbar"/>
+      <input type="search" id="searchbar" placeholder="Hva ser du etter?">
    </div>
-   <div class="userprofile">
-      <img src="placeholder.png"/>
+   <div class="logIn">
+      <img src="./images/profilePicPlaceholder.png"/>
       Logg inn
    </div>
 </div>
