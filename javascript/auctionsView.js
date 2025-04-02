a5cb2e4f2c3bf022cb25dfc6433682b7f75048a7
KristianHeggdal <kristianheggdal@hotmail.no>
2025-04-02 11:17:01 +0200

    AuctionsView og AuctionsviewStyle

--- javascript/auctionsView.js
+++ javascript/auctionsView.js
@@ -7,9 +7,40 @@ function createAuctionsView(){
     createAuctionsView()
 
    function showauctions(){
-    let html='';
-    for (i=0;i<4;i++){
-        html = model.data.listings[0].title;
+        let html = `<h1>Kunstkista</h1>
+    
+    <h2>Auksjonsoversikt</h2>
+    <h3>Auksjon avsluttes: <span id="countdown">1 dag 10 timer 2 minutter</span></h3>
+    
+    
+    
+    
+    <p>Dato: 3. april kl 18:00</p>
+    <p>Dato: 10. april kl 19:00</p>
+    <p>Dato: 17. april kl 18:00</p>
+    <h2>Pågående auksjoner</h2>
+    `
+
+        let listings = model.data.listings;
+        let imgUrl = '';
+     
+        for (let i = 0; i < listings.length; i ++) {
+           if (model.data.listingImages[i].listingsId == listings.id) {
+              imgUrl = model.data.listingImages[i].url;
+           }
+     
+           html += `
+           <div class="auctions-container" id="auctions">
+              <div class="artContainer">
+                 <img src="${imgUrl}"/>
+                 <p>${listings[i].title}</p>
+                 <p>${listings[i].price},-</p>
+                 <div id="bid-button"> 
+                 <button>ballefrans</button>
+                 </div>
+              </div>
+              </div>
+              `
+        }
         return html;
-    }
    }
--- javascript/view.js
+++ javascript/view.js
@@ -1,5 +1,5 @@
 function updateView() {
    document.getElementById('app').innerHTML = /*HTML*/`
    ${createHeader()}
-   ${showHomeView()}`;
+   ${showauctions()}`;
 }
