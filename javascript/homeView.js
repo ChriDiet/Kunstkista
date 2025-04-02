7a8249e4906a4c4c2b09c67782665f9cfc989884
Christina <ch.dietrichs@yahoo.no>
2025-04-02 14:35:13 +0200

    Add homeview html

--- javascript/homeView.js
+++ javascript/homeView.js
@@ -4,15 +4,7 @@ function showHomeView() {
       <p class="auctionCounting">Auksjon avsluttes: 1 dag 10 timer 2 minutter</p>
       <p class="auctionSubtitle">Produkter til auksjon denne uken</p>
       <div class="auctionImgContainer">
-         <div>
-            <div> < </div>
-            <img src="./images/placeholder.png"/>
-            <div>></div>
-         </div>
-         <div class="info">
-            <p>Produktnavn</p>
-            <p>Startpris: 200kr</p>
-         </div>
+         
       </div>
    </div>
    <h2>Nye kunstverk</h2>
@@ -29,8 +21,9 @@ function createNewArtContainer() {
    let html = '';
    let listings = model.data.listings;
    let imgUrl = '';
+   let newListings = 8;
 
-   for (let i = 0; i < listings.length; i ++) {
+   for (let i = 0; i < newListings; i ++) {
       if (model.data.listingImages[i].listingsId == listings.id) {
          imgUrl = model.data.listingImages[i].url;
       }
@@ -44,7 +37,45 @@ function createNewArtContainer() {
    }
    return html;
 }
+//Ikke ferdig//
+function createAuctionSliderHtml() {
+   let html = '';
+   let auctions = model.data.auctions
+   let listingImages = model.data.listingImages;
+   let listings = model.data.listings;
+   let imgUrl = '';
+   let slidesQty = auctions.length;
+   let title = '';
+   let price = '';
+   let sliderImages = [];
+   
+
+   for (let i = 0; i < slidesQty; i ++) {
+      if (listingImages[i].listingsId == auctions.listingId) {
+         imgUrl = listingImages[i].url;
+         title = listings[i].title;
+         price = listings[i].price; 
+         sliderImages.push({imgUrl, title,price})
+      }
+   }
+   console.log(sliderImages);
+}
 
 // function getImageUrl() {
 
+// }
+
+// function createAuctionSlider() {
+
+   
+
+//    <div>
+//             <button> &#10094; </button>
+//             <img src="./images/placeholder.png"/>
+//             <button> &#10095; </button>
+//          </div>
+//          <div class="info">
+//             <p>Produktnavn</p>
+//             <p>Startpris: 200kr</p>
+//          </div>
 // }
