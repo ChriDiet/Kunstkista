abcc51324422d3c47859ffbfb62c2f68403d543f
Christina <ch.dietrichs@yahoo.no>
2025-04-04 11:19:11 +0200

    Add categoriesView

---
+++ javascript/products/categoriesView.js
@@ -0,0 +1,40 @@
+function showCategoriesView() {
+   return /*HTML*/ `
+      <div class="categoryContainer">
+         <div class="largeFont">Kategorier</div>
+   <div class="categoryGrid">
+      <div class="categoryCard 1">
+         <div>Malerier</div>
+         <img src="./images/monalisa.jpg">
+      </div>
+      <div class="categoryCard 2">
+         <div>Strikketøy</div>
+         <img src="./images/mariusgenser.jpg">
+      </div>
+      <div class="categoryCard 3">
+         <div>Trefigurer</div>
+         <img src="./images/isbjorner.jpg">
+      </div>
+      <div class="categoryCard 4">
+         <div>Dreide Boller</div>
+         <img src="./images/dreidboller.jpg">
+      </div>
+      <div class="categoryCard 5">
+         <div>Rosemaling</div>
+         <img src="./images/rosemaling.jpg">
+      </div>
+      <div class="categoryCard 6">
+         <div>Fotografier</div>
+         <img src="./images/fotografier.jpg">
+      </div>
+      <div class="categoryCard 7">
+         <div>Noveller/Skriv</div>
+         <img src="./images/writings.jpg">
+      </div>
+      <div class="categoryCard 8">
+         <div>Treverk</div>
+         <img src="./images/woodwork.jpg">
+      </div>
+   </div>
+   `;
+}
