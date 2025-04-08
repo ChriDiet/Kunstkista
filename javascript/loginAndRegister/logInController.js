e59fbb51995a8ba6a47a67a8d68b6998f0df0870
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-04-08 14:47:10 +0200

    Arbeid på login controller
    logIn controller er under arbeid.

--- javascript/loginAndRegister/logInController.js
+++ javascript/loginAndRegister/logInController.js
@@ -1,10 +1,14 @@
 function logIn(){
-    if(model.input.logIn.userName === model.data.users.userName && model.input.logIn.password === model.data.users.password){
-        model.data.users.isLoggedIn = true;
-        setPage('home');
-    } else{
-        model.data.users.isLoggedIn = false;
-        alert('Brukernavn eller passord');
+   
+    for(let users of model.data.users){
+        if(model.input.logIn.userName === users.userName && model.input.logIn.password === users.password){
+            users.isLoggedIn = true;
+            alert('Velkommen')
+            setPage('home')
+
+        } else{
+            alert('Brukernavn eller passord er feil')
+        }
     }
    
 
