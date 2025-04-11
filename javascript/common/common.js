d43d5b6937da3af6e4c7edc37bbf89235282f2d1
Christina <ch.dietrichs@yahoo.no>
2025-04-11 13:29:52 +0200

    Add functions to get next id

--- javascript/common/common.js
+++ javascript/common/common.js
@@ -1,9 +1,28 @@
 function getNextUserId() {
    let maxId = 0;
-
    for (const user of model.data.users) {
       if (user.id > maxId) {
          maxId = user.id;
+      }
+   }
+   return maxId + 1;
+}
+
+function getNextListingId() {
+      let maxId = 0;
+      for (const listing of model.data.listings) {
+         if (listing.id > maxId) {
+            maxId = listing.id;
+         }
+      }
+      return maxId + 1;
+   }
+
+function getNextContactInfoId() {
+   let maxId = 0;
+   for (const userContactInfo of model.data.userContactInfos) {
+      if (userContactInfo.id > maxId) {
+         maxId = userContactInfo.id;
       }
    }
    return maxId + 1;
