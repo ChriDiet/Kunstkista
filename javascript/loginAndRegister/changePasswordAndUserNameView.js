ca273f8327bcb3e97dc83d42dd66ae1f54c79754
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-04-22 11:26:27 +0200

    Update changePasswordAndUserNameView.js

--- javascript/loginAndRegister/changePasswordAndUserNameView.js
+++ javascript/loginAndRegister/changePasswordAndUserNameView.js
@@ -3,10 +3,10 @@ function drawChangePasswordAndUserNameHtml(){
     <h2>Bytt brukernavn eller passord</h2>
     <div class="passwordAndUserNameContainer">
         <div>
-        <img class="loginImage" src="images/profilePicPlaceHolder.png">
-        <input placeholder="Nytt brukernavn"><br/>
-        <input placeholder="Nytt passord"><br/>
-        <button onclick="changeUserNameAndPassword()">Bekreft</button>
+            <img class="loginImage" src="images/profilePicPlaceHolder.png">
+            <input placeholder="Nytt brukernavn"><br/>
+            <input placeholder="Nytt passord"><br/>
+            <button onclick="changeUserNameAndPassword()">Bekreft</button>
         </div>
     </div>
     `;
