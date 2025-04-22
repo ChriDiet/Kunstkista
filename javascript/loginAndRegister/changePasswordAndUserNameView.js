267539a056efb271eb964cd15c22c973247da2a8
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-04-22 12:36:08 +0200

    ChangePasswordAndUserName
    Lagde controller funksjon og controller fil. Har ferdig stil viewet.

--- javascript/loginAndRegister/changePasswordAndUserNameView.js
+++ javascript/loginAndRegister/changePasswordAndUserNameView.js
@@ -4,8 +4,21 @@ function drawChangePasswordAndUserNameHtml(){
     <div class="passwordAndUserNameContainer">
         <div>
             <img class="loginImage" src="images/profilePicPlaceHolder.png">
-            <input placeholder="Nytt brukernavn"><br/>
-            <input placeholder="Nytt passord"><br/>
+            <input 
+                oninput="model.input.editUser.newUserName = this.value" 
+                placeholder="Nytt brukernavn"
+                value="${model.input.editUser.newUserName}"
+                ><br/>
+            <input 
+                oninput="model.input.editUser.newPassword = this.value" 
+                placeholder="Nytt passord"
+                value="${model.input.editUser.newPassword}"    
+                ><br/>
+            <input 
+                oninput="model.input.editUser.repeatNewPassword = this.value" 
+                placeholder="Repeter nytt passord"
+                value="${model.input.editUser.repeatNewPassword}"
+            ><br/>
             <button onclick="changeUserNameAndPassword()">Bekreft</button>
         </div>
     </div>
