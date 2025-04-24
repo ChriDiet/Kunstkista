d229e60eb8c69877a37c4177e4b0c6f38671afd4
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-04-24 10:32:06 +0200

    Update changePasswordController.js

--- javascript/loginAndRegister/changePasswordController.js
+++ javascript/loginAndRegister/changePasswordController.js
@@ -1,10 +1,12 @@
 function changePassword(){
-    const userLoggedIn = model.data.users.find(user => user.isLoggedIn);
-    const indexOfLoggedInUser = model.data.users.indexOf(userLoggedIn);
-    if(userLoggedIn){
-        model.data.users[indexOfLoggedInUser].password = model.input.editUser.newPassword && model.input.editUser.repeatNewPassword;
-        
+    const user = model.data.users.find(loggedInUser => loggedInUser.userName === model.data.users.userName)
+    if(user){
+        model.app.isLoggedIn = user.id;
+        user.password = model.input.editUser.newPassword && model.input.editUser.repeatNewPassword;
+        setPage('login');
     }
+
+
 
     
     
