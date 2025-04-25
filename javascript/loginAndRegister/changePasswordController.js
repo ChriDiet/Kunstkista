0daa8a6f3c3ef25ec6b45ca63fddf9deb7d641bc
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-04-25 10:37:24 +0200

    Changepassword is almost done
    Almost done with change password.

--- javascript/loginAndRegister/changePasswordController.js
+++ javascript/loginAndRegister/changePasswordController.js
@@ -2,18 +2,19 @@
 //Her skal det lages en funksjon som bytter passord.
 
 function changePassword(){
-    const user = model.data.users.find(loggedInUser => loggedInUser.userName === model.data.users.userName)
-    if(user){
-        model.app.isLoggedIn = user.id;
-        user.password = model.input.editUser.newPassword && model.input.editUser.repeatNewPassword;
-        setPage('login');
+   const user = model.data.users.find(user => user.isLoggedIn === true);
+    if(user.password !== model.input.editUser.newPassword && model.input.editUser.newPassword === model.input.editUser.repeatNewPassword){
+        user.password = model.input.editUser.newPassword;
+        setPage('logIn');
+      }
     }
+        
+
 
 
 
     
     
-}
 
 
 
