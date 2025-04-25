90b1b060ce5b84d479c357c5b4ffa2ad5e2cf5cd
DanielLarsen-spec <larsen97daniel@gmail.com>
2025-04-25 14:59:49 +0200

    ProfileView

--- javascript/userProfiles/profileView.js
+++ javascript/userProfiles/profileView.js
@@ -1,31 +1,45 @@
 function showProfileView(){
     let loggedInUser = '';
+    let profileUser = '';
+    let typeOfButton = '';
+    let typeOfPage = '';
     const user = model.data.users.find(users => users.id == model.app.isLoggedIn);
-    if(user){
-        loggedInUser = `${user.firstName} ${user.lastName}`; 
-    }
+    const userContactInfo = model.data.userContactInfos.find(user => user.id == model.app.isLoggedIn);
     
+    if(user){
+        loggedInUser = `${user.firstName} ${user.lastName}` 
+    }
+    profileUser = model.data.users.find(user => user.id == model.app.isLoggedIn);
+    
+    if(profileUser){
+        typeOfPage = 'contactInfo';
+        typeOfButton = "Rediger profil";
+    } else{
+        typeOfPage = 'editProfile';
+        typeOfButton = 'KontaktInformasjon';
+    }
+       
     return /*HTML*/`
-        <h2>${loggedInUser}</h2>
-        <img class="loginImage" src="images/profilePicPlaceHolder.png">
-        <div class="profileInfoContainer">
-            <div class="profileInfo">
-                <h3>Brukernavn</h3>
-                <p>${user.username}</p>
-            </div>
-            <div class="profileInfo">
-                <h3>Fornavn</h3>
-                <p>${user.firstName}</p>
-            </div>
-            <div class="profileInfo">
-                <h3>Etternavn</h3>
-                <p>${user.lastName}</p>
-            </div>
-                <div class="profileInfo">
-                    <h3>Epost</h3>
-                <p>${user.email}</p>
-            </div>
+    <div class="profileInfoContainer">
+    <h2>${loggedInUser}</h2><br/>
+    <img class="loginImage" src="images/profilePicPlaceHolder.png">
+        <div class="profileInfo">
+            <div>${userContactInfo.city} ${user.rating}</div><br/>
+            <button onclick="setPage(typeOfPage)">${typeOfButton}</button>
+            <h3>Bio:</h3>
+            <div>${user.personalBio}</div> 
+            
+           
+                
         </div>
+                
+            
+            
+            
+            
+            
+            
+           
             
             
             
